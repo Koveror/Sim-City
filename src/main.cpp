d31c5c4ae72cd7345c538391f2863777c8dbdef0
Ilari Rauhala <ilari@Ilari-MacBook-Pro.local>
2017-11-23 16:18:06 +0200

    add dummy images and ability to draw textures

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,15 @@
 #include "graph.hpp"
 #include "vehicle.hpp"
 
+
+
 //Placeholder datastructures for nodes in graph
 std::vector<sf::CircleShape> nodes;     //Store shapes directly for drawing?
 std::vector<sf::Vector2f> coords;       //Store coordinates and make shapes for drawing on the fly?
 
 //Force coordinates to a grid
 int forceToGrid(int a) {
+    /*
     int ma;
     if((a % 16) < 8) {
         ma = a - (a % 16);
@@ -20,6 +23,8 @@ int forceToGrid(int a) {
         ma = a - (a % 16) + 16;
     }
     return ma;
+    */
+    return a / 64 * 64;
 }
 
 //Adds nodes to the vector
@@ -33,6 +38,20 @@ int addNode(int x, int y) {
 }
 
 int main(void) {
+
+    //load textures
+    sf::Texture grass;
+    sf::Texture building;
+
+    if (!grass.loadFromFile("img/grass.png")){
+      std::cout << "grass.png not found" << std::endl;
+    }
+    if (!building.loadFromFile("img/building.png")){
+      std::cout << "building.png not found" << std::endl;
+    }
+
+
+
     //Testing classes
     Vertex testVertex = Vertex(0, 0, "Grass");
     std::cout << "testVertex coord: " << testVertex.getCoord().first << ", "
@@ -44,12 +63,14 @@ int main(void) {
     vehicles.push_back(&cr);
     vehicles.push_back(&tr);
 
-    for (auto it : vehicles) {    
+    for (auto it : vehicles) {
         std::cout << "vehicle type: " << it->getType() << " and length: " << it->getLength() << std::endl;
     }
 
 
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "Sim-City");
+
+
 
     //GUI loop
     while (window.isOpen())
@@ -94,12 +115,20 @@ int main(void) {
             int x = vector.x;
             int y = vector.y;
             int radius = 4;
-            sf::CircleShape node(radius);
-            node.setOrigin(radius, radius);
-            node.setFillColor(sf::Color::Green);
+            //sf::CircleShape node(radius);
+            //node.setOrigin(radius, radius);
+            //node.setFillColor(sf::Color::Green);
+            //node.setPosition(x, y);
+            sf::Sprite node;
+            node.setTexture(grass);
             node.setPosition(x, y);
             window.draw(node);  //Draw all of the nodes to the screen
         }
+
+        sf::Sprite g;
+        g.setTexture(grass);
+        window.draw(g);
+
         //Show it
         window.display();
     }
